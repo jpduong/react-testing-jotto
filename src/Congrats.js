7780b8c971bc7bf0538f2b8cad6cc874de4bdae1
James <james.duong93@gmail.com>
2020-02-23 21:56:01 +1100

    add proptypes for congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * @function
@@ -6,7 +7,7 @@ import React from "react";
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
  */
 
-export default props => {
+const Congrats = props => {
   if (props.success) {
     return (
       <div data-test="component-congrats">
@@ -19,3 +20,9 @@ export default props => {
     return <div data-test="component-congrats" />;
   }
 };
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
+
+export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import { findByTestAttr } from "../test/testUtils";
+import checkPropTypes from "check-prop-types";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component
@@ -13,11 +16,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
 
   expect(component.length).toBe(1);
@@ -35,4 +39,9 @@ test("renders non-empty congrats message when `success` prop is true", () => {
   const message = findByTestAttr(wrapper, "congrats-message");
 
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
