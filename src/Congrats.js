c250e30af606bb1a0c59eefaee2e2db047228051
James <james.duong93@gmail.com>
2020-02-23 21:41:19 +1100

    add congrats component and tes utils

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,8 +2,20 @@ import React from "react";
 
 /**
  * @function
- * @returns {JSX.Element} - Rendered component (or null if `success` prop)
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
  */
-export default () => {
-  return <div></div>;
+
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessted the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
