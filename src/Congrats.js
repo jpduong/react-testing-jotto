dd69d91058f6b9c58b4184d158801d72c64592d5
James <james.duong93@gmail.com>
2020-02-24 19:38:58 +1100

    bootstrap css added

--- src/Congrats.js
+++ src/Congrats.js
@@ -10,9 +10,9 @@ import PropTypes from "prop-types";
 const Congrats = props => {
   if (props.success) {
     return (
-      <div data-test="component-congrats">
+      <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
-          Congratulations! You guessted the word!
+          Congratulations! You guessed the word!
         </span>
       </div>
     );
