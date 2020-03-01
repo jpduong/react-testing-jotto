b5bed17f387a3a8d8339e820ff8d2a557e52dff9
James Duong <james.duong93@gmail.com>
2020-03-02 10:09:49 +1100

    strings test boilerplate

---
+++ src/helpers/strings.test.js
@@ -0,0 +1,16 @@
+import stringsModule from './strings';
+const { getStringByLanguage } = stringsModule;
+
+test('returns correct submit string for english', () => {
+
+});
+
+test('returns the correct submit string for emoji', () => {
+
+});
+
+test('returns english submit string when language does not exist',() => {
+
+}
+
+test('returns english submit string when submit key does not exist for language', () => {})
